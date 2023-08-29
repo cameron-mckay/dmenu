28e3522716bb6880b462e227d99a6bb27c09e760
Cameron McKay <cameron@webnx.com>
2023-08-28 18:01:01 -0600

    Changed colors

--- config.h
+++ config.h
@@ -5,7 +5,7 @@ static int topbar = 1;                      /* -b  option; if 0, dmenu appears a
 /* -fn option overrides fonts[0]; default X11 font or font set */
 static const char *fonts[] = {
 	"monospace:size=10",
-	"NotoColorEmoji:pixelsize=8:antialias=true:autohint=true"
+	"NotoColorEmoji:pixelsize=25:antialias=true:autohint=true"
 };
 static const unsigned int bgalpha = 0xe0;
 static const unsigned int fgalpha = OPAQUE;
@@ -13,8 +13,8 @@ static const char *prompt      = NULL;      /* -p  option; prompt to the left of
 static const char *colors[SchemeLast][2] = {
 	/*     fg         bg       */
 	[SchemeNorm] = { "#bbbbbb", "#222222" },
-	[SchemeSel] = { "#eeeeee", "#005577" },
-	[SchemeOut] = { "#000000", "#00ffff" },
+	[SchemeSel] = { "#eeeeee", "#30ab4f" },
+	[SchemeOut] = { "#000000", "#9d00ff" },
 };
 static const unsigned int alphas[SchemeLast][2] = {
 	/*		fgalpha		bgalphga	*/
